e3797329e6587716a6ea91311bba7b784a868bd1
Aiden Egglin <uefadude@gmail.com>
2025-04-18 23:49:15 -0400

    Add key listener

---
+++ src/KListener.java
@@ -0,0 +1,28 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KListener implements KeyListener {
+
+    // 128 ASCII characters
+    private boolean keyPressed[] = new boolean[128];
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        keyPressed[e.getKeyCode()] =  true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        keyPressed[e.getKeyCode()] = false;
+    }
+
+    public boolean isKeyPressed(int keyCode) {
+        return keyPressed[keyCode];
+    }
+}
--- src/Window.java
+++ src/Window.java
@@ -1,9 +1,12 @@
 import javax.swing.JFrame;
-import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
 
 public class Window extends JFrame implements Runnable {
 
     private Graphics2D g2d;
+    private KListener keyListener = new KListener();
 
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
@@ -11,12 +14,19 @@ public class Window extends JFrame implements Runnable {
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(keyListener);
         g2d = (Graphics2D)this.getGraphics();
     }
 
     public void update(double dt) {
         g2d.setColor(Color.BLACK);
         g2d.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+
+        if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
+            System.out.println("User pressed up.");
+        } else if (keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
+            System.out.println("Down");
+        }
     }
 
     @Override
